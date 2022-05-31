eb1639c30f06d2b47078d2bc4721133faaf65f82
bravo680 <iambravo680@gmail.com>
2022-05-31 19:06:36 +0700

    change scss variavles to css variables

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import App from './App'
-import store from './store/store'
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import App from "./App";
+import store from "./store/store";
+import "./index.scss";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Provider store={ store}>
+      <Provider store={store}>
         <App />
       </Provider>
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
